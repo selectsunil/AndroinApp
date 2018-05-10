3e0af69577b1624ae2b118ad2545da521b2a5391
Rinku <soubhagyapradhan87@gmail.com>
2018-05-10 13:35:22 +0530

    added navigation clickevents from right to left

--- app/src/main/java/com/androin/androinapp/Home2.java
+++ app/src/main/java/com/androin/androinapp/Home2.java
@@ -1,24 +1,137 @@
 package com.androin.androinapp;
 
-import android.app.SearchManager;
-import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
-import android.view.Menu;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
-import android.widget.Button;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Home2 extends AppCompatActivity {
+public class Home2 extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    ImageView srchbtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home2);
-        srchbtn=findViewById(R.id.srchbtn);
 
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "You have chosen mail option", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ImageButton menuLeft = (ImageButton) findViewById(R.id.menuLeft);
+        ImageView menuRight = (ImageView) findViewById(R.id.menuRight);
+        ImageView srchbtn = (ImageView) findViewById(R.id.srchbtn);
+
+//        menuLeft.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (drawer.isDrawerOpen(GravityCompat.START)) {
+//                    drawer.closeDrawer(GravityCompat.START);
+//                } else {
+//                    drawer.openDrawer(GravityCompat.START);
+//                }
+//            }
+//        });
+        menuRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (drawer.isDrawerOpen(GravityCompat.END)) {
+                    drawer.closeDrawer(GravityCompat.END);
+                } else {
+                    drawer.openDrawer(GravityCompat.END);
+                }
+            }
+        });
+
+
+
+//        NavigationView navigationView1 = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView2 = (NavigationView) findViewById(R.id.nav_view2);
+//        navigationView1.setNavigationItemSelectedListener(this);
+        navigationView2.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        String text = "";
+//        if (id == R.id.nav_camera) {
+//            text = "camera";
+//        } else if (id == R.id.nav_gallery) {
+//            text = "gallery";
+//        } else if (id == R.id.nav_slideshow) {
+//            text = "slideshow";
+//        } else if (id == R.id.nav_manage) {
+//            text = "tools";
+//        } else if (id == R.id.nav_share) {
+//            text = "share";
+//        } else if (id == R.id.nav_send) {
+//            text = "send";
+//        }
+        if (id == R.id.nav_home) {
+            text = "home";
+        } else if (id == R.id.nav_gal) {
+            text = "gallery";
+        } else if (id == R.id.nav_share) {
+            text = "share";
+        }
+        Toast.makeText(this, "You have chosen " + text, Toast.LENGTH_LONG).show();
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+        drawer.closeDrawer(GravityCompat.END);
+        return true;
     }
 }
